70c8effc7a7f0dde9e804f2df532deef9f496877
Trung Hieu <trunghieu2570@gmail.com>
2020-04-04 19:08:52 +0700

    add SearchBar component

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -7,7 +7,7 @@ import Footer from '../Footer';
 export default class Home extends Component {
     render() {
         return (
-            <div class="home">
+            <div className="home">
                 <div className="head">
                     <TopNav />
                     <MenuNav />
